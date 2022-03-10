774122ed458448863fca6ff44997ecff6df5078c
William Yu <wy427@nyu.edu>
2022-03-10 16:57:50 -0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      <Button
+        title="Go to Image Demo"
+        onPress={() => navigation.navigate("Image")}
+      />
+      <Button
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate("Counter")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
