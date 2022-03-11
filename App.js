7accc3fd53f9916f832e5c7c54bef5a6c996cf57
William Yu <wy427@nyu.edu>
2022-03-10 23:04:57 -0500

    added more screens and functionality

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-
+import ColorScreen from "./src/screens/ColorScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import SquareScreen from "./src/screens/SquareScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
   },
   {
     initialRouteName: "Home",
---
+++ src/components/ColorCounter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+  return (
+    <View>
+      <Text>{color}</Text>
+      <Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+      <Button title={`Decrease ${color}`} onPress={() => onDecrease()} />
+    </View>
+  );
+};
+
+export default ColorCounter;
